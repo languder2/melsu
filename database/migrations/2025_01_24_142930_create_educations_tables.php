35faa115b4fe1d549471eea45ffe11da0565386f
languder2 <163087417+languder2@users.noreply.github.com>
2025-01-24 16:23:31 +0300

    w->h

--- database/migrations/2025_01_24_142930_create_educations_tables.php
+++ database/migrations/2025_01_24_142930_create_educations_tables.php
@@ -11,103 +11,156 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('education_faculties', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('code')->unique()->nullable();
-            $table->string('logo')->nullable();
-            $table->string('image')->nullable();
-            $table->longText('description')->nullable();
+        if(!Schema::hasTable('education_faculties'))
+            Schema::create('education_faculties', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('code')->unique();
+                $table->longText('description')->nullable();
 
-            $table->string('sort')->default(1000);
-            $table->timestamp('deleted_at')->nullable();
-            $table->timestamps();
-        });
+                $table->string('sort')->default(1000);
+                $table->timestamp('deleted_at')->nullable();
+                $table->timestamps();
+            });
 
-        Schema::create('education_department_types', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('code')->unique();
+        if(!Schema::hasTable('education_department_types'))
+            Schema::create('education_department_types', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('code')->unique();
 
-            $table->string('sort')->default(1000);
-            $table->timestamp('deleted_at')->nullable();
-            $table->timestamps();
-        });
+                $table->string('sort')->default(1000);
+                $table->timestamp('deleted_at')->nullable();
+                $table->timestamps();
+            });
 
-        Schema::create('education_departments', function (Blueprint $table) {
+        if(!Schema::hasTable('education_departments'))
+            Schema::create('education_departments', function (Blueprint $table) {
 
-            $table->id();
-            $table->string('name');
-            $table->string('code')->unique();
-            $table->unsignedBigInteger('type_code');
-            $table->unsignedBigInteger('parent_id');
-            $table->string('logo')->nullable();
-            $table->string('image')->nullable();
-            $table->longText('description')->nullable();
+                $table->id();
+                $table->string('name');
+                $table->string('code')->unique();
+                $table->string('faculty_code')->nullable();
+                $table->string('type_code')->nullable();
+                $table->unsignedBigInteger('parent_id')->nullable();
+                $table->longText('description')->nullable();
 
-            $table->string('sort')->default(1000);
-            $table->timestamp('deleted_at')->nullable();
-            $table->timestamps();
-        });
+                $table->string('sort')->default(1000);
+                $table->timestamp('deleted_at')->nullable();
+                $table->timestamps();
 
-        Schema::create('education_levels', function (Blueprint $table) {
-            $table->id();
+                $table->foreign('parent_id')
+                    ->references('id')
+                    ->on('education_departments')
+                    ->cascadeOnUpdate()
+                    ->nullOnDelete();
 
-            $table->string('name');
-            $table->string('code')->unique();
+                $table->foreign('faculty_code')
+                    ->references('code')
+                    ->on('education_faculties')
+                    ->cascadeOnUpdate()
+                    ->nullOnDelete();
 
-            $table->integer('sort')->default(10000);
-            $table->timestamp('deleted_at')->nullable();
-            $table->timestamps();
-        });
+                $table->foreign('type_code')
+                    ->references('code')
+                    ->on('education_department_types')
+                    ->cascadeOnUpdate()
+                    ->nullOnDelete();
+            });
 
-        Schema::create('education_specialities', function (Blueprint $table) {
-            $table->id();
+        if(!Schema::hasTable('education_levels'))
+            Schema::create('education_levels', function (Blueprint $table) {
+                $table->id();
 
-            $table->string('name');
-            $table->string('code')->unique();
-            $table->string('faculty_code');
-            $table->string('department_code');
-            $table->string('level_code');
-            $table->json('places');
-            $table->longText('description')->nullable();
+                $table->string('name');
+                $table->string('code')->unique();
 
-            $table->integer('sort')->default(10000);
+                $table->integer('sort')->default(10000);
+                $table->timestamp('deleted_at')->nullable();
+                $table->timestamps();
+            });
 
-            $table->timestamp('deleted_at')->nullable();
-            $table->timestamps();
-        });
+        if(!Schema::hasTable('education_specialities'))
+            Schema::create('education_specialities', function (Blueprint $table) {
+                $table->id();
 
-        Schema::create('education_form_types', function (Blueprint $table) {
-            $table->id();
+                $table->string('name');
+                $table->string('code')->unique();
+                $table->string('faculty_code')->nullable();
+                $table->string('department_code')->nullable();
+                $table->string('level_code')->nullable();
+                $table->json('places');
+                $table->longText('description')->nullable();
 
-            $table->string('name');
-            $table->longText('description')->nullable();
-            $table->string('speciality_code')->nullable();
-            $table->decimal('terms', 2, 1)->nullable();
-            $table->json('places')->nullable();
-            $table->json('scores')->nullable();
-            $table->json('exams')->nullable();
-            $table->string('director')->nullable();
-            $table->text('address')->nullable();
-            $table->string('director')->nullable();
-            $table->string('afc')->comment('admission of foreign citizens')->nullable();
-            $table->integer('price')->nullable();
+                $table->integer('sort')->default(10000);
 
-            $table->timestamp('deleted_at')->nullable();
-            $table->timestamps();
-        });
+                $table->timestamp('deleted_at')->nullable();
+                $table->timestamps();
 
-        Schema::create('education_forms', function (Blueprint $table) {
-            $table->id();
+                $table->foreign('faculty_code')
+                    ->references('code')
+                    ->on('education_faculties')
+                    ->cascadeOnUpdate()
+                    ->nullOnDelete();
 
-            $table->string('name');
-            $table->string('type_code');
-            $table->string('speciality_code');
+                $table->foreign('department_code')
+                    ->references('code')
+                    ->on('education_departments')
+                    ->cascadeOnUpdate()
+                    ->nullOnDelete();
 
-            $table->timestamp('deleted_at')->nullable();
-            $table->timestamps();
-        });
+                $table->foreign('level_code')
+                    ->references('code')
+                    ->on('education_levels')
+                    ->cascadeOnUpdate()
+                    ->nullOnDelete();
+            });
+
+        if(!Schema::hasTable('education_form_types'))
+            Schema::create('education_form_types', function (Blueprint $table) {
+                $table->id();
+
+                $table->string('name');
+                $table->string('code')->unique();
+
+                $table->timestamp('deleted_at')->nullable();
+                $table->timestamps();
+            });
+
+        if(!Schema::hasTable('education_forms'))
+            Schema::create('education_forms', function (Blueprint $table) {
+                $table->id();
+
+                $table->string('name');
+                $table->longText('description')->nullable();
+                $table->string('speciality_code')->nullable();
+                $table->string('type_code')->nullable();
+                $table->decimal('terms', 2, 1)->nullable();
+                $table->json('places')->nullable();
+                $table->json('scores')->nullable();
+                $table->json('exams')->nullable();
+                $table->string('director')->nullable();
+                $table->text('address')->nullable();
+                $table->string('afc')->nullable()
+                    ->comment('admission of foreign citizens');
+                $table->integer('price')->nullable();
+
+                $table->timestamp('deleted_at')->nullable();
+                $table->timestamps();
+
+                $table->foreign('speciality_code')
+                    ->references('code')
+                    ->on('education_specialities')
+                    ->cascadeOnUpdate()
+                    ->nullOnDelete();
+
+                $table->foreign('type_code')
+                    ->references('code')
+                    ->on('education_form_types')
+                    ->cascadeOnUpdate()
+                    ->nullOnDelete();
+
+            });
 
     }
 
@@ -116,12 +169,14 @@ return new class extends Migration
      */
     public function down(): void
     {
+
+
         Schema::dropIfExists('education_forms');
         Schema::dropIfExists('education_form_types');
         Schema::dropIfExists('education_specialities');
         Schema::dropIfExists('education_levels');
-        Schema::dropIfExists('education_department_types');
         Schema::dropIfExists('education_departments');
+        Schema::dropIfExists('education_department_types');
         Schema::dropIfExists('education_faculties');
     }
 };
