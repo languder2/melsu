b5efb9d051b06b214a41a7bba6bfa53c5da75da9
Максим Сын Отца Своего <noimxl1@gmail.com>
2025-02-17 15:19:22 +0300

    add page "view-melsu-this-is"

---
+++ resources/views/nomix/view-melsu-this-is.blade.php
@@ -0,0 +1,118 @@
+<section class="main-section">
+        <div class="lg:py-24">
+            <div class="grid grid-cols-1 lg:grid-cols-[1fr_auto_1fr]">
+                <div class="left-info-box flex justify-between flex-col order-2 lg:order-1">
+                    <div class="info-hex-wrapp flex justify-end">
+                        <div class="info-hex-content">
+                            <a href="https://melsu.ru/novorossiya" class="group ">
+                            <h3 class="font-semibold mb-2 group-hover:text-red-900 transition duration-300 ease-linear">Новороссия</h3>
+                            <p class="text-base font-normal line-clamp-4 mb-2">
+                                Культурное наследие Новороссии включает в себя разнообразие традиций, языков и архитектурных стилей.
+                                Местные праздники, музыка и кухня отображают многогранность этих влияний.
+                            </p>
+                                <span class="group-hover:text-red-900 transition duration-300 ease-linear font-semibold">Подробнее</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="info-hex-wrapp flex justify-end">
+                        <div class="info-hex-content center-info-content">
+                            <a href="https://melsu.ru/zaporozhskaya-oblast" class="group ">
+                                <h3 class="font-semibold mb-2 group-hover:text-red-900 transition duration-300 ease-linear">Запорожская область</h3>
+                                <p class="text-base font-normal line-clamp-4 mb-2">
+                                    это сердце юга России!<br>
+                                    Регион является значимым промышленным и аграрным центром, где успешно развиваются металлургия, машиностроение, переработка сельскохозяйственной продукции и энергетика.
+                                </p>
+                                <span class="group-hover:text-red-900 transition duration-300 ease-linear font-semibold">Подробнее</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="info-hex-wrapp flex justify-end">
+                        <div class="info-hex-content">
+                            <a href="https://melsu.ru/melitopol" class="group ">
+                                <h3 class="font-semibold mb-2 group-hover:text-red-900 transition duration-300 ease-linear">Мелитополь</h3>
+                                <p class="text-base font-normal line-clamp-4 mb-2">
+                                    Мелитополь славится своим теплым климатом, близостью к курортноым зонам, а также развитым энергетическим сектором и технологическим потенциалом.
+                                </p>
+                                <span class="group-hover:text-red-900 transition duration-300 ease-linear font-semibold">Подробнее</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="middle-block-hex relative order-1 lg:order-2">
+                    <div class="left-top-line hidden lg:flex">
+                    </div>
+                    <div class="right-top-line hidden lg:flex">
+                    </div>
+                    <div class="left-center-line hidden lg:flex">
+                    </div>
+                    <div class="right-center-line hidden lg:flex">
+                    </div>
+                    <div class="left-bottom-line hidden lg:flex">
+                    </div>
+                    <div class="right-bottom-line hidden lg:flex">
+                    </div>
+                    <div class="hex">
+                        <div class="hexinner">
+                            <div class="hex-content-box">
+                                <div class="hex-content">
+                                    <div class="hex-logo">
+                                        <img src="{{asset('img/bg-white-logo.png')}}" alt="">
+                                    </div>
+                                    <h2 class="text-white font-semibold">МелГУ Это -</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="left-top-img">
+                        <img src="{{asset('img/circle-1.jpg')}}" alt="">
+                    </div>
+                    <div class="right-top-img">
+                        <img src="{{asset('img/circle-2.jpg')}}" alt="">
+                    </div>
+                    <div class="left-bottom-img">
+                        <img src="{{asset('img/circle-3.png')}}" alt="">
+                    </div>
+                    <div class="right-bottom-img">
+                        <img src="{{asset('img/circle-4.jpg')}}" alt="">
+                    </div>
+                </div>
+                <div class="right-info-box flex justify-between flex-col order-3">
+                    <div class="info-hex-wrapp flex justify-start">
+                        <div class="info-hex-content">
+                            <a href="https://melsu.ru/forpost-russkih-tekhnologij" class="group ">
+                                <h3 class="font-semibold mb-2 group-hover:text-red-900 transition duration-300 ease-linear">Форпост русских технологий</h3>
+                                <p class="text-base font-normal line-clamp-4 mb-2">
+                                    МелГУ — форпост продвижения и внедрения инновационных образовательных программ и высоких технологий в Новороссии,
+                                    ключевой фактор социально-экономического развития региона!
+                                </p>
+                                <span class="group-hover:text-red-900 transition duration-300 ease-linear font-semibold">Подробнее</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="info-hex-wrapp flex justify-start">
+                        <div class="info-hex-content center-info-content">
+                            <a href="https://melsu.ru/klyuch-k-uspekhu" class="group ">
+                                <h3 class="font-semibold mb-2 group-hover:text-red-900 transition duration-300 ease-linear">Ключ к успеху</h3>
+                                <p class="text-base font-normal line-clamp-4 mb-2">
+                                    В МелГУ представлены 4 основные области знаний: естественно-научная, гуманитарная, инженерно-техническая, медико-биологическая.
+                                </p>
+                                <span class="group-hover:text-red-900 transition duration-300 ease-linear font-semibold">Подробнее</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="info-hex-wrapp flex justify-start">
+                        <div class="info-hex-content">
+                            <a href="https://melsu.ru/elita-russkogo-mira" class="group ">
+                                <h3 class="font-semibold mb-2 group-hover:text-red-900 transition duration-300 ease-linear">Элита русского мира</h3>
+                                <p class="text-base font-normal line-clamp-4 mb-2">
+                                   В МелГУ студенты становятся чемпионами мира, состоят в молодежных организациях Российской Федерации,
+                                    проходят практику на предприятиях и получают возможность трудоустройства по всей стране!
+                                </p>
+                                <span class="group-hover:text-red-900 transition duration-300 ease-linear font-semibold">Подробнее</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
