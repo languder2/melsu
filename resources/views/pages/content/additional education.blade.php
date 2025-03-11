53a46226fb43dd255d9ac2183511df4b1e7ed7a8
Максим Сын Отца Своего <noimxl1@gmail.com>
2025-03-11 15:42:33 +0300

    linking pages

---
+++ resources/views/pages/content/additional education.blade.php
@@ -0,0 +1,47 @@
+<p class="mb-3 bg-white p-6">
+    Дополнительное образование открывает двери к непрерывному развитию и профессиональному росту.<br><br>
+    Это не просто возможность получить новые знания, но и шанс изменить свою жизнь, открыть для себя новые горизонты и реализовать скрытый потенциал.
+    Дополнительное образование становится мощным инструментом для тех, кто стремится к успеху, хочет оставаться конкурентоспособным и готов инвестировать в свое будущее.
+</p>
+<div class="sved-block">
+    <div class="upper-sved-block p-4 flex justify-center">
+        <h2 class="uppercase text-white font-bold text-center text-2xl xl:text-5xl w-full xl:leading-[66px]">Дополнительное образование — <br>мост к новым навыкам</h2>
+    </div>
+    <div class="relative grid grid-cols-1 lg:grid-cols-2 bg-red-900 text-white text-sm sm:text-lg py-7 px-12 w-full lg:mx-none gap-3">
+        <video autoplay loop muted class="vid-bg">
+            <source src="{{asset('video/anim-logo.webm')}}" type="video/webm">
+        </video>
+        <div class="z-10">
+            <ul class="text-base xl:text-lg list-outside list-disc">
+                <li class="leading-normal pb-3">
+                    <a class="transition duration-300 ease-in-out hover:opacity-60" href="https://melsu.ru/division/23">Институт непрерывного образования</a>
+                </li>
+                <li class="leading-normal pb-3">
+                    <a class="transition duration-300 ease-in-out hover:opacity-60" href="https://melsu.ru/normativnye-pravovye-dokumenty-dopolnitelnoe-obrazovanie">Нормативные правовые документы</a>
+                </li>
+                <li class="leading-normal pb-3">
+                    <a class="transition duration-300 ease-in-out hover:opacity-60" href="https://melsu.ru/programmy-professionalnoj-perepodgotovki-dopolnitelnoe-obrazovanie">Программы профессиональной переподготовки</a>
+                </li>
+                <li class="leading-normal pb-3">
+                    <a class="transition duration-300 ease-in-out hover:opacity-60" href="https://melsu.ru/programmy-realizuemye-v-federal'nom-proekte-sodejstvie-zanyatosti">Программы реализуемые в Федеральном проекте «Содействие занятости»</a>
+                </li>
+            </ul>
+        </div>
+        <div class="z-10">
+            <ul class="text-base xl:text-lg list-outside list-disc">
+                <li class="leading-normal pb-3">
+                    <a class="transition duration-300 ease-in-out hover:opacity-60" href="https://melsu.ru/lokalnye-normativnye-akty-dopolnitelnoe-obrazovanie">Локальные нормативные акты</a>
+                </li>
+                <li class="leading-normal pb-3">
+                    <a class="transition duration-300 ease-in-out hover:opacity-60" href="https://melsu.ru/programmy-povysheniya-kvalifikacii-dopolnitelnoe-obrazovanie">Программы повышения квалификации</a>
+                </li>
+                <li class="leading-normal pb-3">
+                    <a class="transition duration-300 ease-in-out hover:opacity-60" href="https://melsu.ru/programmy-professionalnogo-obucheniya-dopolnitelnoe-obrazovaniya">Программы профессионального обучения</a>
+                </li>
+                <li class="leading-normal pb-3">
+                    <a class="transition duration-300 ease-in-out hover:opacity-60" href="https://melsu.ru/update-page">Сетевые образовательные программы РАНХиГС</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
---
+++ resources/views/pages/content/innovative-educational-technologies.blade.php
@@ -0,0 +1,39 @@
+<p class="mb-3 bg-white p-6">
+    Инновационные образовательные технологии в университете направлены на создание современной и гибкой образовательной среды, которая отвечает вызовам цифровой эпохи.
+    Они включают использование электронных учебных платформ, онлайн-курсов, виртуальных лабораторий и интерактивных методов обучения.
+    Эти технологии позволяют студентам получать знания в удобном формате, развивать цифровые навыки и активно участвовать в образовательном процессе,
+    а преподавателям — эффективно управлять учебными материалами и адаптировать программы под индивидуальные потребности обучающихся.<br><br>
+    Кроме того, университет активно внедряет проектный подход, междисциплинарные программы и технологии смешанного обучения (blended learning), сочетающие онлайн- и офлайн-форматы.
+    Это способствует развитию критического мышления, креативности и практических навыков у студентов, готовя их к успешной профессиональной деятельности в быстро меняющемся мире.
+    Инновации также поддерживают международное сотрудничество, позволяя студентам и преподавателям участвовать в глобальных образовательных проектах и обмениваться опытом.
+</p>
+<div class="sved-block">
+    <div class="upper-sved-block p-4 flex justify-center">
+        <h2 class="uppercase text-white font-bold text-center text-2xl xl:text-5xl w-full xl:leading-[66px]">Инновационные образовательные технологии — <br>ключ к будущему</h2>
+    </div>
+    <div class="relative grid grid-cols-1 lg:grid-cols-2 bg-red-900 text-white text-sm sm:text-lg py-7 px-12 w-full lg:mx-none gap-3">
+        <video autoplay loop muted class="vid-bg">
+            <source src="{{asset('video/anim-logo.webm')}}" type="video/webm">
+        </video>
+        <div class="z-10">
+            <ul class="text-base xl:text-lg list-outside list-disc">
+                <li class="leading-normal pb-3">
+                    <a class="transition duration-300 ease-in-out hover:opacity-60" href="https://melsu.ru/elektronnaya-informacionno-obrazovatelnaya-sreda">Электронная информационно-образовательная среда (ЭИОС)</a>
+                </li>
+                <li class="leading-normal pb-3">
+                    <a class="transition duration-300 ease-in-out hover:opacity-60" href="https://melsu.ru/individualnaya-obrazovatelnaya-traektoriya">Индивидуальная образовательная траектория (ИОТ)</a>
+                </li>
+            </ul>
+        </div>
+        <div class="z-10">
+            <ul class="text-base xl:text-lg list-outside list-disc">
+                <li class="leading-normal pb-3">
+                    <a class="transition duration-300 ease-in-out hover:opacity-60" href="https://melsu.ru/startap-kak-diplom">Стартап как диплом</a>
+                </li>
+                <li class="leading-normal pb-3">
+                    <a class="transition duration-300 ease-in-out hover:opacity-60" href="https://melsu.ru/update-page">Электронное дистанционное обучение</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
