a9edec5d0ce5708d9f2de4b6c7d37c18fc3842cc
languder2 <163087417+languder2@users.noreply.github.com>
2025-03-11 08:22:16 +0300

    w->h

---
+++ app/Models/Education/FacultyCopy.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Models\Education;
+
+use App\Models\Contact;
+use App\Models\Division\Division;
+use App\Models\Education\Department as EducationDepartment;
+use App\Models\Gallery\Image;
+use App\Models\Page\Content as PageContent;
+use App\Models\Staff\Affiliation as StaffAffiliation;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class FacultyCopy extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'education_faculties';
+
+    protected $fillable = [
+        'id',
+        'acronym',
+        'type',
+        'name',
+        'code',
+        'description',
+        'order',
+        'show',
+    ];
+
+    public static function FormRules($id): array
+    {
+        return [
+//            'test'              => "required",
+            'acronym'           => 'nullable',
+            'type'              => '',
+            'name'              => 'required',
+            'code'              => "required|unique:education_faculties,code,{$id},id,deleted_at,NULL",
+            'description'       => '',
+            'order'             => 'nullable|numeric',
+            'image'             => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'preview'           => '',
+            'show'              => '',
+            'chief'             => '',
+            'staffs'            => '',
+            'sections'          => '',
+        ];
+    }
+
+    public static function FormMessage(): array
+    {
+        return [
+            'name' => 'Укажите заголовок',
+            'code.required' => 'Код должен быть указан',
+            'code.unique' => 'Код должен быть уникальным',
+        ];
+    }
+
+    public function departments(): HasMany
+    {
+        return $this->hasMany(EducationDepartment::class, 'faculty_code', 'code')
+            ->orderBy('order', 'desc')
+            ->orderBy('name');
+    }
+
+    public function labs(): HasMany
+    {
+        return $this->hasMany(EducationDepartment::class, 'faculty_code', 'code')
+            ->where('type_code','lab')
+            ->orderBy('order', 'desc')
+            ->orderBy('name');
+    }
+
+    public function specialities(): HasMany
+    {
+        return $this->hasMany(Speciality::class, 'faculty_code', 'code')
+            ->orderBy('order', 'desc')
+            ->orderBy('spec_code');
+    }
+
+    public function getOrderAttribute($order): int|null
+    {
+        return ($order < 10000) ? $order :  null ;
+    }
+
+    public function setOrderAttribute($order): void
+    {
+        $this->attributes['order'] = $order ?? 10000;
+    }
+
+    public function image(): MorphMany
+    {
+        return $this->morphMany(Image::class, 'relation');
+    }
+
+    public function logo(): MorphOne
+    {
+
+        $image = $this->MorphOne(Image::class, 'relation')->where('type', 'logo');
+
+        if(!$this->id)
+            return $image;
+
+        if(!$image->count())
+            $image->create([
+                'type'      => 'logo',
+                'name'      => 'preview',
+            ]);
+
+        return $image;
+    }
+
+    public function staffs($all= false): MorphMany
+    {
+
+        $response = $this->morphMany(StaffAffiliation::class, 'relation')->orderBy('order');
+
+        if(!$all)
+            $response = $response->where('type','staff');
+
+        return $response;
+    }
+    public function chief(): MorphOne
+    {
+        return $this->MorphOne(StaffAffiliation::class, 'relation')->where('type','chief');
+    }
+
+    public function sections(): MorphMany
+    {
+        return $this->morphMany(PageContent::class, 'relation');
+    }
+
+    public function contacts(?string $type = null): MorphMany
+    {
+        $query = $this->morphMany(Contact::class, 'relation');
+
+        if($type)
+            $query->where('type', $type);
+
+        return $query->orderBy('type')->orderBy('sort');
+    }
+
+    public function getLinkAttribute(): string
+    {
+        return route('public:education:faculty',[
+            $this->code ?? $this->id ?? null
+        ]);
+    }
+
+    public function department():MorphOne
+    {
+        return $this->morphOne(Division::class, 'relation');
+    }
+
+
+}
+
+
---
+++ resources/views/components/form/sections/staffs.blade.php
@@ -0,0 +1,66 @@
+<div
+    id="tab_staffs"
+    @class([
+        "form_box",
+        (old('side_menu') === 'tab_staffs')?'':'hidden'
+    ])
+>
+
+    <div class="bg-stone-50 p-4 flex gap-4 mb-4">
+        <div class="flex-1 text-lg font-semibold">
+            Сотрудники
+        </div>
+        <div class="w-8 text-center">
+            <a href="{{route('api:department:staff:add-position')}}"
+               class="
+                inline-block mt-2
+                hover:text-green-700
+                active:text-gray-700
+            "
+               onClick="Actions.addStaffPosition(this,'staffs'); return false;"
+               title="Добавить позицию сотрудника"
+            >
+                <i class="fas fa-user-plus"></i>
+            </a>
+
+        </div>
+
+    </div>
+
+    <div id="staffs" class="flex flex-col gap-4 mb-4">
+
+        <x-staff.select-chief
+            :id='$current?->chief?->id ?? null '
+            :chief='$current?->chief?->staff_id ?? null '
+            :post="$current?->chief?->post ?? null "
+            :post_alt="$current?->chief?->post_alt ?? null "
+            name="chief"
+            :old="old('chief')"
+        />
+
+
+        @if(old('_token'))
+            @foreach(old('staffs') as $staff_id => $staff)
+                <x-staff.select-with-post
+                    :id="$staff_id"
+                />
+            @endforeach
+        @elseif($current->staffs ?? null)
+            @foreach($current->staffs as $staff)
+                <x-staff.select-with-post
+                    :id="$staff->id"
+                    :staff='$staff->staff_id'
+                    :post='$staff->post'
+                    :post_alt='$staff->post_alt'
+                    :order='$staff->order'
+                />
+            @endforeach
+        @endif
+
+        @if(!old('_token') && (!$current || $current->staffs->count()===0))
+            <x-staff.select-with-post
+                :id="(int)microtime(true)"
+            />
+        @endif
+    </div>
+</div>
