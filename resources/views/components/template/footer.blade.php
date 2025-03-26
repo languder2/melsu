1002502e45a54de89c944a89d7f4bd74be28ba6a
Максим Сын Отца Своего <59877317+Segizmund@users.noreply.github.com>
2025-03-26 11:02:59 +0300

    change footer links

--- resources/views/components/template/footer.blade.php
+++ resources/views/components/template/footer.blade.php
@@ -11,24 +11,24 @@
             <div class="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-3 text-white items-start">
                 <div class="grid grid-cols-1 h-full">
                     <h4 class="text-[#B9BAC1] text-md mb-3">Приемная ректора</h4>
-                    <a href="tel:79901469279" class="text-lg font-[600] mb-2 hover:opacity-80 transition duration-300 ease-linear">+7(990) 146-92-79</a>
-                    <a href="https://yandex.ru/maps/10367/melitopol/house/ulitsa_beybulatova_2/Z0wYdQBgSkEAQFpsfXR1dXpjbQ==/?ll=35.370347%2C46.844609&z=17" class="text-lg font-[600] mb-2 hover:opacity-80 transition duration-300 ease-linear min-h-[56px] flex items-center">г. Мелитополь, ул. Бейбулатова 2, каб. 15.302</a>
+                    <a href="tel:79901469279" class="text-lg font-[600] mb-2 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">+7(990) 146-92-79</a>
+                    <a href="https://yandex.ru/maps/10367/melitopol/house/ulitsa_beybulatova_2/Z0wYdQBgSkEAQFpsfXR1dXpjbQ==/?ll=35.370347%2C46.844609&z=17" class="text-lg font-[600] mb-2 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear min-h-[56px] flex items-center">г. Мелитополь, ул. Бейбулатова 2, каб. 15.302</a>
                     <h4 class="text-[#B9BAC1] text-md my-3">Для писем</h4>
-                    <a href="mailto:rektorat@mgu-mlt.ru" class="text-lg font-[600] mb-2 hover:opacity-80 transition duration-300 ease-linear">rektorat@mgu-mlt.ru</a>
+                    <a href="mailto:rektorat@mgu-mlt.ru" class="text-lg font-[600] mb-2 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">rektorat@mgu-mlt.ru</a>
                 </div>
                 <div class="grid grid-cols-1 h-full">
                     <h4 class="text-[#B9BAC1] text-md mb-3">Приемная комиссия</h4>
-                    <a href="tel:79900215716" class="text-lg font-[600] mb-2 hover:opacity-80 transition duration-300 ease-linear">+7(990) 021-57-16</a>
-                    <a href="https://yandex.ru/maps/org/melitopolskiy_gosudarstvenny_pedagogicheskiy_universitet_imeni_bogdana_khmelnitskogo_glavny_korpus/15156338654/?ll=35.378279%2C46.842316&z=17" class="text-lg font-[600] mb-2 hover:opacity-80 transition duration-300 ease-linear min-h-[56px] flex items-center">г. Мелитополь, ул. Гетманская 20, каб. 2.114</a>
+                    <a href="tel:79900215716" class="text-lg font-[600] mb-2 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">+7(990) 021-57-16</a>
+                    <a href="https://yandex.ru/maps/org/melitopolskiy_gosudarstvenny_pedagogicheskiy_universitet_imeni_bogdana_khmelnitskogo_glavny_korpus/15156338654/?ll=35.378279%2C46.842316&z=17" class="text-lg font-[600] mb-2 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear min-h-[56px] flex items-center">г. Мелитополь, ул. Гетманская 20, каб. 2.114</a>
                     <h4 class="text-[#B9BAC1] text-md my-3">Для писем</h4>
-                    <a href="mailto:priyomnaya_komissiya@mgu-mlt.ru" class="text-lg font-[600] mb-2 hover:opacity-80 transition duration-300 ease-linear">priyomnaya_komissiya@mgu-mlt.ru</a>
+                    <a href="mailto:priyomnaya_komissiya@mgu-mlt.ru" class="text-lg font-[600] mb-2 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">priyomnaya_komissiya@mgu-mlt.ru</a>
                 </div>
                 <div class="grid grid-cols-1 h-full">
                     <h4 class="text-[#B9BAC1] text-md mb-3">Пресс-служба</h4>
-                    <a href="tel:79901427865" class="text-lg font-[600] mb-2 hover:opacity-80 transition duration-300 ease-linear">+7(990) 142-78-65</a>
-                    <a href="https://yandex.ru/maps/10367/melitopol/house/prospekt_bogdana_khmelnitskogo_18/Z0wYdQBjS0QFQFpsfXR1d35iZQ==/?ll=35.373212%2C46.846212&z=17" class="text-lg font-[600] mb-2 hover:opacity-80 transition duration-300 ease-linear min-h-[56px] flex items-center">г. Мелитополь, пр. Богдана Хмельницкого 18, аудитория: 1.116</a>
+                    <a href="tel:79901427865" class="text-lg font-[600] mb-2 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">+7(990) 142-78-65</a>
+                    <a href="https://yandex.ru/maps/10367/melitopol/house/prospekt_bogdana_khmelnitskogo_18/Z0wYdQBjS0QFQFpsfXR1d35iZQ==/?ll=35.373212%2C46.846212&z=17" class="text-lg font-[600] mb-2 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear min-h-[56px] flex items-center">г. Мелитополь, пр. Богдана Хмельницкого 18, аудитория: 1.116</a>
                     <h4 class="text-[#B9BAC1] text-md my-3">Для писем</h4>
-                    <a href="mailto:press_slujba@mgu-mlt.ru" class="text-lg font-[600] mb-2 hover:opacity-80 transition duration-300 ease-linear">press_slujba@mgu-mlt.ru</a>
+                    <a href="mailto:press_slujba@mgu-mlt.ru" class="text-lg font-[600] mb-2 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">press_slujba@mgu-mlt.ru</a>
                 </div>
                 <!--<div class="grid grid-cols-1">
                     <h4 class="text-[#B9BAC1] text-md mb-3">Приемная ректора</h4>
@@ -53,40 +53,40 @@
                     Федеральное государственное бюджетное образовательное учреждение высшего образования «Мелитопольский государственный университет»
                 </p>
             </div>
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 text-white">
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 text-white">
                 <div class="grid grid-cols-1">
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">Сведения об образовательной организации</a>
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">МелГУ в рейтингах</a>
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">Инклюзивное образование</a>
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">Аккредитация специалистов</a>
+                    <a href="{{url('sved')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Сведения об образовательной организации</a>
+                    <a href="{{url('history')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">История МелГУ</a>
+                    <a href="{{url('rectorate')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Ректорат</a>
+                    <a href="{{url('divisions')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Структура Университета</a>
                 </div>
                 <div class="grid grid-cols-1">
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">Сведения об образовательной организации</a>
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">МелГУ в рейтингах</a>
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">Инклюзивное образование</a>
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">Аккредитация специалистов</a>
+                    <a href="{{url('obrazovatelnaya-infrastruktura')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Образовательная инфраструктура</a>
+                    <a href="{{url('schedule/show')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Расписание занятий</a>
+                    <a href="{{url('https://melsu.ru/division/anti-corruption-department')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Противодействие коррупции</a>
+                    <a href="{{url('https://melsu.ru/division/154')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Инклюзивное образование</a>
                 </div>
                 <div class="grid grid-cols-1">
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">Сведения об образовательной организации</a>
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">МелГУ в рейтингах</a>
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">Инклюзивное образование</a>
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">Аккредитация специалистов</a>
+                    <a href="{{url('https://edu.gov.ru/')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Минпросвещения России</a>
+                    <a href="{{url('https://minobrnauki.gov.ru/')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Минобрнауки России</a>
+                    <a href="{{url('nauchnye-issledovaniya-universiteta')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Научные исследования университета</a>
+                    <a href="{{url('nauchnye-kruzhki')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Научно-исследовательская работа студентов</a>
                 </div>
                 <div class="grid grid-cols-1">
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">Сведения об образовательной организации</a>
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">МелГУ в рейтингах</a>
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">Инклюзивное образование</a>
-                    <a href="#" class="text-md mb-4 hover:opacity-80 transition duration-300 ease-linear">Аккредитация специалистов</a>
+                    <a href="{{url('struktura-i-komanda')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Структура и команда</a>
+                    <a href="{{url('granty-i-forumy')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Гранты и форумы</a>
+                    <a href="{{url('obedineniya-i-kluby')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Объединения и клубы</a>
+                    <a href="{{url('centr-kompetencij')}}" class="text-md mb-4 hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Центр компетенций</a>
                 </div>
             </div>
         </div>
     </div>
     <div class="bg-[#252738] py-6 text-white">
         <div class="container custom lg:p-2.5 flex flex-col lg:flex-row gap-3">
-            <a href="#" class="text-md">Карта сайта</a>
-            <a href="#" class="text-md">Почтовый сервер</a>
-            <a href="#" class="text-md">Пользовательское соглашение</a>
-            <a href="#" class="text-md">Политика по обработке персональных данных</a>
+            <a href="{{url('campus-map')}}" class="text-md hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Карта сайта</a>
+            <a href="#" class="text-md hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Почтовый сервер</a>
+            <a href="#" class="text-md hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Пользовательское соглашение</a>
+            <a href="#" class="text-md hover:opacity-80 hover:text-red-200 w-fit transition duration-300 ease-linear">Политика по обработке персональных данных</a>
         </div>
     </div>
 </footer>
