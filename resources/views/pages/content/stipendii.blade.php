0945a27da4f2cf6cff6887de479d834425a6ff06
Максим Сын Отца Своего <59877317+Segizmund@users.noreply.github.com>
2025-03-27 15:32:54 +0300

    edit page stipendii.blade.php

--- resources/views/pages/content/stipendii.blade.php
+++ resources/views/pages/content/stipendii.blade.php
@@ -1,4 +1,254 @@
 <section>
+    <h2 class="text-xl mb-3 font-semibold">Размеры стипендий и компенсационных выплат для обучающихся по очной форме обучения за счет бюджетных
+        ассигнований федерального бюджета с 01.02.2025 г.</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-[3fr_1fr] mb-3">
+        <div class="col-span-2 p-3 border-l border-r border-t text-center">
+            <h3 class="font-semibold">1. Размер государственной академической стипендии (руб.)</h3>
+        </div>
+        <div class="flex items-center p-3 border col-span-2 sm:col-span-1">
+            Студентам первого года обучения до первого
+            семестрового контроля
+        </div>
+        <div class="flex items-center p-3 border-e border-l sm:border-l-0 sm:border-t border-b col-span-2 sm:col-span-1 sm:ms-0 justify-center sm:justify-start">
+            4 500,00
+        </div>
+        <div class="flex flex-col justify-center p-3 border-l border-r border-b col-span-2 sm:col-span-1">
+            Студентам имеющим по итогам промежуточной
+            аттестации оценки «отлично» и (или) «хорошо», с учетом
+            среднего бала:
+            <ul class="ms-4 list-disc">
+                <li>от 4,0 до 4,75</li>
+                <li>от 4,75 (включительно) до 5,00 (второй степени) </li>
+                <li>равен 5,00 (первой степени) </li>
+            </ul>
+        </div>
+        <div class="flex flex-col sm:justify-between p-3 border-e border-l sm:border-l-0 sm:border-t border-b col-span-2 sm:col-span-1 sm:ms-0 justify-center">
+
+                <div class="flex flex-col">
+                    <span class="text-center sm:text-start">-</span>
+                </div>
+                <div class="flex flex-col">
+                    <span class="text-center sm:text-start">4 500,00</span>
+                    <span class="text-center sm:text-start">5 400,00</span>
+                    <span class="text-center sm:text-start">6 300,00</span>
+                </div>
+        </div>
+
+        <div class="flex items-center p-3 border-l border-r border-b col-span-2 sm:col-span-1">
+            Студентам первого и второго курсов, обучающимся по
+            образовательным программам высшего образования
+            (программам бакалавриата, программам
+            специалитета), имеющим, по итогам промежуточной
+            аттестации, оценки успеваемости «отлично» или
+            «хорошо» или «отлично» и «хорошо» и
+            являющимся студентами в возрасте до 20 лет,
+            имеющими только одного родителя - инвалида I группы
+        </div>
+        <div class="flex items-center p-3 border-e border-l sm:border-l-0 sm:border-t border-b col-span-2 sm:col-span-1 sm:ms-0 justify-center sm:justify-start">
+            14 700,00
+        </div>
+
+        <div class="col-span-2 p-3 border-l border-r border-b text-center">
+            <h3 class="font-semibold">2. Размер повышенной государственной академической стипендии (руб.)</h3>
+        </div>
+        <div class="flex items-center p-3 border-l border-r border-b">
+            Студентам, имеющим достижения в какой-либо одной
+            или нескольких областях деятельности (учебной,
+            научно-исследовательской, общественной, культурнотворческой и спортивной)
+        </div>
+        <div class="flex items-center p-3 border-e border-l sm:border-l-0 sm:border-t border-b col-span-2 sm:col-span-1 sm:ms-0 justify-center sm:justify-start">
+            14 375,00(письмо
+            МН-11/57 от
+            20.01.25)
+        </div>
+
+        <div class="col-span-2 p-3 border-l border-r border-b text-center">
+            <h3 class="font-semibold">3. Размер государственной социальной стипендии (руб.) для ВПО</h3>
+        </div>
+        <div class="flex flex-col justify-center p-3 border-l border-r border-b col-span-2 sm:col-span-1">
+            Студентам, которые являются:
+            <ul class="ms-4 list-disc">
+                <li>детьми-сиротами и детьми, оставшимися без
+                    попечения родителей;</li>
+                <li>лицами из числа детей-сирот и детей, оставшихся
+                    без попечения родителей;</li>
+                <li>лицами в возрасте от 18 до 23 лет, потерявшими
+                    в период обучения обоих родителей или
+                    единственного родителя;</li>
+                <li>детьми-инвалидами, инвалидами I и II групп,
+                    инвалидами с детства;</li>
+                <li>лицами, подвергшимися воздействию радиации
+                    вследствие катастрофы на Чернобыльской АЭС и
+                    иных радиационных катастроф, вследствие
+                    ядерных испытаний на Семипалатинском
+                    полигоне;</li>
+                <li>лицами, являющимися инвалидами вследствие
+                    военной травмы или заболеваний, полученных в
+                    период прохождения военной службы;</li>
+                <li>лицами, являющимися ветеранами боевых
+                    действий; лицами из числа граждан, проходивших
+                    в течение не менее трёх лет военную службу по
+                    контракту на воинских должностях, подлежащих
+                    замещению солдатами, матросами, сержантами,
+                    старшинами, и уволенных с военной службы по
+                    основаниям, предусмотренным подпунктами «б» -
+                    «г» пункта 1, подпунктом «а» пункта 2 и
+                    подпунктами «а» - «в» пункта 3 статьи 51
+                    Федерального закона от 28.03.1998 г. № 53 «О
+                    воинской обязанности и военной службе»;</li>
+                <li>получателями государственной социальной
+                    помощи.</li>
+            </ul>
+        </div>
+        <div class="flex items-center p-3 border-e border-l sm:border-l-0 sm:border-t border-b col-span-2 sm:col-span-1 sm:ms-0 justify-center sm:justify-start">
+            8 000,00
+        </div>
+
+        <div class="col-span-2 p-3 border-l border-r border-b text-center">
+            <h3 class="font-semibold">Размер государственной социальной стипендии (руб.) для СПО</h3>
+        </div>
+        <div class="flex flex-col justify-center p-3 border-l border-r border-b col-span-2 sm:col-span-1">
+            Студентам, которые являются:
+            <ul class="ms-4 list-disc">
+                <li>детьми-сиротами и детьми, оставшимися без
+                    попечения родителей;</li>
+                <li>лицами из числа детей-сирот и детей, оставшихся
+                    без попечения родителей;</li>
+                <li>лицами в возрасте от 18 до 23 лет, потерявшими
+                    в период обучения обоих родителей или
+                    единственного родителя;</li>
+                <li>детьми-инвалидами, инвалидами I и II групп,
+                    инвалидами с детства;</li>
+                <li>лицами, подвергшимися воздействию радиации
+                    вследствие катастрофы на Чернобыльской АЭС и
+                    иных радиационных катастроф, вследствие
+                    ядерных испытаний на Семипалатинском
+                    полигоне;</li>
+                <li>лицами, являющимися инвалидами вследствие
+                    1 119,00
+                    военной травмы или заболеваний, полученных в
+                    период прохождения военной службы;</li>
+                <li>лицами, являющимися ветеранами боевых
+                    действий; лицами из числа граждан, проходивших
+                    в течение не менее трёх лет военную службу по
+                    контракту на воинских должностях, подлежащих
+                    замещению солдатами, матросами, сержантами,
+                    старшинами, и уволенных с военной службы по
+                    основаниям, предусмотренным подпунктами «б» -
+                    «г» пункта 1, подпунктом «а» пункта 2 и
+                    подпунктами «а» - «в» пункта 3 статьи 51
+                    Федерального закона от 28.03.1998 г. № 53 «О
+                    воинской обязанности и военной службе»;</li>
+                <li>получателями государственной социальной
+                    помощи.</li>
+            </ul>
+        </div>
+        <div class="flex items-center p-3 border-e border-l sm:border-l-0 sm:border-t border-b col-span-2 sm:col-span-1 sm:ms-0 justify-center sm:justify-start">
+            1 119,00
+        </div>
+
+        <div class="col-span-2 p-3 border-l border-r border-b text-center">
+            <h3 class="font-semibold">4. Размер повышенной государственной социальной стипендии (руб.)</h3>
+        </div>
+        <div class="flex items-center p-3 border-l border-r border-b col-span-2 sm:col-span-1">
+            Студентам первого и второго курсов, обучающимся по
+            образовательным программам высшего образования
+            (программам бакалавриата, программам специалитета),
+            имеющим оценки успеваемости «отлично» или
+            «хорошо» или «отлично» и «хорошо» и относящимся
+            к категориям лиц, указанным в п. 3. данного
+            Приложения
+        </div>
+        <div class="flex items-center p-3 border-e border-l sm:border-l-0 sm:border-t border-b col-span-2 sm:col-span-1 sm:ms-0 justify-center sm:justify-start">
+            14 375,00(письмо
+            МН-11/57 от
+            20.01.25)
+        </div>
+
+        <div class="col-span-2 p-3 border-l border-r border-b text-center">
+            <h3 class="font-semibold">5. Размер государственной стипендии аспирантам (руб.)</h3>
+        </div>
+        <div class="flex items-center p-3 border-l border-r border-b col-span-2 sm:col-span-1">
+            По программам подготовки научно-педагогических
+            кадров в аспирантуре (за исключением
+            государственной стипендии аспирантам,
+            обучающимся по образовательным программам
+            подготовки научно-педагогических кадров по
+            направлениям подготовки, определенным
+            Минобрнауки России)
+
+        </div>
+        <div class="flex items-center p-3 border-e border-l sm:border-l-0 sm:border-t border-b col-span-2 sm:col-span-1 sm:ms-0 justify-center sm:justify-start">
+            9 000,00
+        </div>
+
+        <div class="col-span-2 p-3 border-l border-r border-b text-center">
+            <h3 class="font-semibold">6. Размер государственной академической стипендии (руб.) для СПО</h3>
+        </div>
+        <div class="flex items-center p-3 border-l border-r border-b col-span-2 sm:col-span-1">
+            Студентам имеющим по итогам промежуточной
+            аттестации оценки «отлично» и (или) «хорошо»
+        </div>
+        <div class="flex items-center p-3 border-e border-l sm:border-l-0 sm:border-t border-b col-span-2 sm:col-span-1 sm:ms-0 justify-center sm:justify-start">
+            745,00
+        </div>
+
+        <div class="col-span-2 p-3 border-l border-r border-b text-center">
+            <h3 class="font-semibold">7. Размер компенсации на приобретение питания в день(руб.)</h3>
+        </div>
+        <div class="flex items-center justify-center col-span-2 p-3 border-l border-e border-b">
+            410,00
+        </div>
+
+        <div class="col-span-2 p-3 border-l border-r border-b text-center">
+            <h3 class="font-semibold">8. Размер компенсации на приобретение одежды,
+                обуви и мягкого инвентаря в год (руб.) </h3>
+        </div>
+        <div class="flex items-center justify-center col-span-2 p-3 border-l border-e border-b">
+            73 688,00
+        </div>
+
+        <div class="col-span-2 p-3 border-l border-r border-b text-center">
+            <h3 class="font-semibold">9. Размер компенсации на приобретение одежды,
+                обуви и мягкого инвентаря ( при выпуске) (руб.)</h3>
+        </div>
+        <div class="flex items-center justify-center col-span-2 p-3 border-l border-e border-b">
+            98 571,00
+        </div>
+
+        <div class="col-span-2 p-3 border-l border-r border-b text-center">
+            <h3 class="font-semibold">10. Размер единовременного денежного пособия
+                (при выпуске) (руб.)
+            </h3>
+        </div>
+        <div class="flex items-center justify-center col-span-2 p-3 border-l border-e border-b">
+            522,00
+        </div>
+
+        <div class="col-span-2 p-3 border-l border-r border-b text-center">
+            <h3 class="font-semibold">11. Размер ежегодного денежного пособия на
+                приобретения учебной литературы и письменных
+                принадлежностей для студентов, обучающихся по
+                программам высшего образования (руб.)</h3>
+        </div>
+        <div class="flex items-center justify-center col-span-2 p-3 border-l border-e border-b">
+            9 534,00
+        </div>
+
+        <div class="col-span-2 p-3 border-l border-r border-b text-center">
+            <h3 class="font-semibold">12. Размер ежегодного денежного пособия на
+                приобретения учебной литературы и письменных
+                принадлежностей для студентов, обучающихся по
+                программам среднего профессионального
+                образования (руб.)
+            </h3>
+        </div>
+        <div class="flex items-center justify-center col-span-2 p-3 border-l border-e border-b">
+            3 452,00
+        </div>
+    </div>
+
+<h2 class="font-semibold text-xl mb-3">Документы</h2>
 <div class="grid grid-cols-1 gap-3">
     <a href="../../../storage/documents/board-of-trustees/polozh-stipendii.pdf" target="_blank" class="flex items-center group transition duration-300 ease-linear hover:text-[#C10F1A]">
         <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="#820000" class="bi bi-file-earmark-pdf-fill group-hover:fill-[#C10F1A] me-2 transition duration-300 ease-linear" viewBox="0 0 16 16">
@@ -6,14 +256,6 @@
             <path fill-rule="evenodd" d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2m5.5 1.5v2a1 1 0 0 0 1 1h2zM4.165 13.668c.09.18.23.343.438.419.207.075.412.04.58-.03.318-.13.635-.436.926-.786.333-.401.683-.927 1.021-1.51a11.7 11.7 0 0 1 1.997-.406c.3.383.61.713.91.95.28.22.603.403.934.417a.86.86 0 0 0 .51-.138c.155-.101.27-.247.354-.416.09-.181.145-.37.138-.563a.84.84 0 0 0-.2-.518c-.226-.27-.596-.4-.96-.465a5.8 5.8 0 0 0-1.335-.05 11 11 0 0 1-.98-1.686c.25-.66.437-1.284.52-1.794.036-.218.055-.426.048-.614a1.24 1.24 0 0 0-.127-.538.7.7 0 0 0-.477-.365c-.202-.043-.41 0-.601.077-.377.15-.576.47-.651.823-.073.34-.04.736.046 1.136.088.406.238.848.43 1.295a20 20 0 0 1-1.062 2.227 7.7 7.7 0 0 0-1.482.645c-.37.22-.699.48-.897.787-.21.326-.275.714-.08 1.103"/>
         </svg>
         Положение о стипендиальном обеспечении
-    </a>
-    <a href="../../../storage/documents/board-of-trustees/razmer-stipendii.pdf" target="_blank" class="flex items-center group transition duration-300 ease-linear hover:text-[#C10F1A]">
-        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="#820000" class="bi bi-file-earmark-pdf-fill group-hover:fill-[#C10F1A] me-2 transition duration-300 ease-linear" viewBox="0 0 16 16">
-            <path d="M5.523 12.424q.21-.124.459-.238a8 8 0 0 1-.45.606c-.28.337-.498.516-.635.572l-.035.012a.3.3 0 0 1-.026-.044c-.056-.11-.054-.216.04-.36.106-.165.319-.354.647-.548m2.455-1.647q-.178.037-.356.078a21 21 0 0 0 .5-1.05 12 12 0 0 0 .51.858q-.326.048-.654.114m2.525.939a4 4 0 0 1-.435-.41q.344.007.612.054c.317.057.466.147.518.209a.1.1 0 0 1 .026.064.44.44 0 0 1-.06.2.3.3 0 0 1-.094.124.1.1 0 0 1-.069.015c-.09-.003-.258-.066-.498-.256M8.278 6.97c-.04.244-.108.524-.2.829a5 5 0 0 1-.089-.346c-.076-.353-.087-.63-.046-.822.038-.177.11-.248.196-.283a.5.5 0 0 1 .145-.04c.013.03.028.092.032.198q.008.183-.038.465z"/>
-            <path fill-rule="evenodd" d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2m5.5 1.5v2a1 1 0 0 0 1 1h2zM4.165 13.668c.09.18.23.343.438.419.207.075.412.04.58-.03.318-.13.635-.436.926-.786.333-.401.683-.927 1.021-1.51a11.7 11.7 0 0 1 1.997-.406c.3.383.61.713.91.95.28.22.603.403.934.417a.86.86 0 0 0 .51-.138c.155-.101.27-.247.354-.416.09-.181.145-.37.138-.563a.84.84 0 0 0-.2-.518c-.226-.27-.596-.4-.96-.465a5.8 5.8 0 0 0-1.335-.05 11 11 0 0 1-.98-1.686c.25-.66.437-1.284.52-1.794.036-.218.055-.426.048-.614a1.24 1.24 0 0 0-.127-.538.7.7 0 0 0-.477-.365c-.202-.043-.41 0-.601.077-.377.15-.576.47-.651.823-.073.34-.04.736.046 1.136.088.406.238.848.43 1.295a20 20 0 0 1-1.062 2.227 7.7 7.7 0 0 0-1.482.645c-.37.22-.699.48-.897.787-.21.326-.275.714-.08 1.103"/>
-        </svg>
-        Размеры стипендий и компенсационных выплат для обучающихся по очной форме обучения за счет бюджетных
-        ассигнований федерального бюджета с 01.02.2025 г.
     </a>
 </div>
 </section>
