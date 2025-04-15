24b901e00cc97fb614dd2180df1accdac5cb5a8d
Максим Сын Отца Своего <59877317+Segizmund@users.noreply.github.com>
2025-04-15 13:46:59 +0300

    change links

--- resources/views/pages/content/graduates.blade.php
+++ resources/views/pages/content/graduates.blade.php
@@ -8,8 +8,8 @@
         </h2>
         <div class="grid grid-cols-1 lg:grid-cols-[1fr_1fr] xl:grid-cols-[1fr_1fr_1fr_1fr] gap-3 lg:gap-5">
             <div class="box-info-link bg-[var(--primary-color)] p-2.5 text-white">
-                <a href="#" class="h-[160px] lg:min-h-[200px] flex flex-col justify-between">
-                    <h2 class="font-[700] text-lg">Перечень документов для поступления</h2>
+                <a href="https://melsu.ru/trudoustrojstvo-vypusknikov-i-studentov" class="h-[160px] lg:min-h-[200px] flex flex-col justify-between">
+                    <h2 class="font-[700] text-lg">Трудоустройство выпускников и студентов</h2>
                     <div class="text-end">
                                 <span class="text-xl font-[400]">
                                     Подробнее
@@ -19,8 +19,8 @@
                 </a>
             </div>
             <div class="box-info-link bg-[var(--secondary-color)] p-2.5 text-white">
-                <a href="#" class="min-h-[160px] lg:min-h-[200px] flex flex-col justify-between">
-                    <h2 class="font-[700] text-lg">Даты вступительной кампании 2025</h2>
+                <a href="https://melsu.ru/division/centr_trudoustroystva_53" class="min-h-[160px] lg:min-h-[200px] flex flex-col justify-between">
+                    <h2 class="font-[700] text-lg">Центр трудоустройства и развития карьеры</h2>
                     <div class="text-end">
                                 <span class="text-xl font-[400]">
                                     Подробнее
@@ -45,7 +45,7 @@
     >
         <div class="grid grid-cols-1 lg:grid-cols-[40%_40%] gap-2.5 lg:gap-5 lg:justify-end">
             <div class="box-info-link bg-[#1E1E1E] p-2.5 text-white">
-                <a href="#" class="h-[160px] lg:min-h-[350px] flex flex-col justify-between">
+                <a href="https://melsu.ru/update-page" class="h-[160px] lg:min-h-[350px] flex flex-col justify-between">
                     <h2 class="font-[700] text-lg">Ассоциация выпускников и амбассадоры МелГУ</h2>
                     <div class="text-end">
                                 <span class="text-xl font-[400]">
@@ -56,7 +56,7 @@
                 </a>
             </div>
             <div class="box-info-link bg-[var(--primary-color)] p-2.5 text-white">
-                <a href="#" class="h-[160px] lg:min-h-[350px] flex flex-col justify-between">
+                <a href="https://melsu.ru/update-page" class="h-[160px] lg:min-h-[350px] flex flex-col justify-between">
                     <h2 class="font-[700] text-lg">Экспертные кейсы для студентов</h2>
                     <div class="text-end">
                                 <span class="text-xl font-[400]">
@@ -67,7 +67,7 @@
                 </a>
             </div>
             <div class="box-info-link bg-[var(--secondary-color)] p-2.5 text-white">
-                <a href="#" class="h-[160px] lg:min-h-[350px] flex flex-col justify-between">
+                <a href="https://melsu.ru/partner" class="h-[160px] lg:min-h-[350px] flex flex-col justify-between">
                     <h2 class="font-[700] text-lg">Знакомство с предприятием-партнером</h2>
                     <div class="text-end">
                                 <span class="text-xl font-[400]">
@@ -78,7 +78,7 @@
                 </a>
             </div>
             <div class="box-info-link bg-[#685858] p-2.5 text-white">
-                <a href="#" class="h-[160px] lg:min-h-[350px] flex flex-col justify-between">
+                <a href="https://melsu.ru/update-page" class="h-[160px] lg:min-h-[350px] flex flex-col justify-between">
                     <h2 class="font-[700] text-lg">Проекты развития МелГУ</h2>
                     <div class="text-end">
                                 <span class="text-xl font-[400]">
@@ -100,7 +100,7 @@
     <div class="container custom p-2.5 lg:p-5">
         <div class="grid grid-cols-1  lg:grid-cols-[1fr_1fr_1fr] gap-3 lg:gap-5">
             <div class="box-info-link bg-[#1E1E1E] p-2.5 text-white">
-                <a href="#" class="h-[160px] lg:min-h-[200px] flex flex-col justify-between">
+                <a href="https://melsu.ru/update-page" class="h-[160px] lg:min-h-[200px] flex flex-col justify-between">
                     <h2 class="font-[700] text-lg">Дипломы, документы, переводы</h2>
                     <div class="text-end">
                                 <span class="text-xl font-[400]">
@@ -111,7 +111,7 @@
                 </a>
             </div>
             <div class="box-info-link bg-[var(--primary-color)] p-2.5 text-white">
-                <a href="#" class="h-[160px] lg:min-h-[200px] flex flex-col justify-between">
+                <a href="https://melsu.ru/update-page" class="h-[160px] lg:min-h-[200px] flex flex-col justify-between">
                     <h2 class="font-[700] text-lg">Музейный комплекс, проведения экскурсии</h2>
                     <div class="text-end">
                                 <span class="text-xl font-[400]">
@@ -122,7 +122,7 @@
                 </a>
             </div>
             <div class="box-info-link bg-[var(--secondary-color)] p-2.5 text-white">
-                <a href="#" class="min-h-[160px] lg:min-h-[200px] flex flex-col justify-between">
+                <a href="https://melsu.ru/update-page" class="min-h-[160px] lg:min-h-[200px] flex flex-col justify-between">
                     <h2 class="font-[700] text-lg">Вакансии для выпускников</h2>
                     <div class="text-end">
                                 <span class="text-xl font-[400]">
