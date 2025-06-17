001958729301d2ff57d6de591d7da6eb02222f72
Максим Сын Отца Своего <59877317+Segizmund@users.noreply.github.com>
2025-06-17 14:49:00 +0300

    new under-ino-slider

--- resources/views/components/menu/mainmenu.blade.php
+++ resources/views/components/menu/mainmenu.blade.php
@@ -81,7 +81,7 @@
                 <div class="search-and-btn-box">
                     <div class="search-in-menu p-[15px] flex items-center gap-3">
                         <div class="bvi-shortcode border-r pr-2.5">
-                            <a href="#" class="bvi-open flex items-center gap-2 text-lg text-[#820000]">
+                            <a href="#" class="bvi-open flex items-center gap-2 text-xl text-[#820000]">
                                 <svg aria-hidden="true" width="25" height="25" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="bvi-svg-eye">
                                     <path fill="currentColor" d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z" class="bvi-svg-eye">
                                     </path>
--- resources/views/components/slider/mainpage.blade.php
+++ resources/views/components/slider/mainpage.blade.php
@@ -67,56 +67,48 @@
         <div class="slider-container">
             <div class="slider-wrapper">
                 <div class="box-info-under-slider ">
-                    <a href="{{ route('clusters.list') }}" class="under-info flex gap-2 items-center">
-                        <span>
-                            <svg width="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M20.663 8.85C21.2522 6.81022 21.0868 5.03955 20.0236 3.97636C17.7744 1.7271 12.3587 3.49602 7.92736 7.92736C7.45397 8.40074 7.01097 8.88536 6.6 9.37615M17.4 14.6238C16.989 15.1146 16.546 15.5993 16.0726 16.0726C11.6413 20.504 6.22562 22.2729 3.97636 20.0236C2.83569 18.883 2.72842 16.928 3.47772 14.7" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                <path d="M12.6734 9.30078L10.9223 11.3711C10.7086 11.6483 10.6017 11.7869 10.6679 11.8938C10.7341 12.0008 10.9268 12.0008 11.3122 12.0008H12.6847C13.0701 12.0008 13.2627 12.0008 13.329 12.1077C13.3952 12.2147 13.2883 12.3533 13.0745 12.6304L11.3122 14.7008" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                <path d="M9.75 17.7059C9.13145 17.2114 8.52058 16.6659 7.92736 16.0726C3.49602 11.6413 1.7271 6.22562 3.97636 3.97636C5.11702 2.83569 7.07202 2.72842 9.3 3.47772M14.25 20.3607C16.6631 21.2813 18.8068 21.2405 20.0236 20.0236C22.2729 17.7744 20.504 12.3587 16.0726 7.92736C15.4794 7.33413 14.8686 6.78862 14.25 6.29407" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
+                    <a href="{{ route('clusters.list') }}" class="under-info flex gap-2 items-center justify-between bg-[#C10F1A]">
+                        <span class="text-under-info font-semibold">
+                            Флагманские проекты
                         </span>
-                        <span class="text-under-info text-2xl font-semibold">
-                        Флагманские проекты
-                    </span>
+                        <div class="ico-link bg-white rounded-full w-[40px] h-[40px] sm:w-[60px] sm:h-[60px] flex items-center justify-center">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3.66667 1H17M17 1V14.3333M17 1L1 17" stroke="#252525" stroke-width="2"/>
+                            </svg>
+                        </div>
                     </a>
-                    <a href="{{url('programma-razvitiya-melsu')}}" class="under-info flex gap-2 items-center">
-                        <span class="icon-box">
-                            <svg aria-hidden="true"
-                                 class="e-font-icon-svg e-far-calendar-alt"
-                                 viewBox="0 0 448 512"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M148 288h-40c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12zm108-12v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 96v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm192 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96-260v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h48V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h128V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h48c26.5 0 48 21.5 48 48zm-48 346V160H48v298c0 3.3 2.7 6 6 6h340c3.3 0 6-2.7 6-6z">
-                                </path>
-                            </svg>
-                        </span>
-                        <span class="text-under-info text-2xl font-semibold">
+                    <a href="{{url('programma-razvitiya-melsu')}}" class="under-info flex gap-2 items-center justify-between bg-[#252525]">
+                        <span class="text-under-info font-semibold">
                             Программа развития МелГУ на 2023-2028 годы
                         </span>
+                        <div class="ico-link bg-white rounded-full w-[40px] h-[40px] sm:w-[60px] sm:h-[60px] flex items-center justify-center">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3.66667 1H17M17 1V14.3333M17 1L1 17" stroke="#252525" stroke-width="2"/>
+                            </svg>
+                        </div>
                     </a>
                 </div>
                 <div class="box-info-under-slider ">
-                    <a href="{{ url('selekcionno-semenovodcheskij-centr-vysokotekhnologichnyj-selekcionno-pitomnikovodcheskij-kompleks-plodovyh-kultur-novorossii') }}" class="under-info flex gap-2 items-center">
-                        <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M22 16.5C22 19.5376 19.5376 22 16.5 22C13.4624 22 11 19.5376 11 16.5C11 13.4624 13.4624 11 16.5 11C19.5376 11 22 13.4624 22 16.5Z" stroke="white" stroke-width="1.5"/>
-                            <path d="M10.5 11C9.62217 10.37 8.55171 10 7.39646 10C4.41608 10 2 12.4624 2 15.5C2 18.5376 4.41608 21 7.39646 21C8.08877 21 8.75062 20.8671 9.35882 20.6251" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
-                            <path d="M16 13C14.1631 11.1035 11.7291 7.13692 13.7946 4M16 2C14.9847 2.59904 14.2703 3.27752 13.7946 4M13.7946 4C11.4006 4.5 6.09142 6.5 7.13408 12" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-
-                        <span class="text-under-info text-2xl font-semibold text-wrap text-center">
+                    <a href="{{ url('selekcionno-semenovodcheskij-centr-vysokotekhnologichnyj-selekcionno-pitomnikovodcheskij-kompleks-plodovyh-kultur-novorossii') }}" class="under-info flex gap-2 items-center justify-between">
+                        <span class="text-under-info font-semibold text-wrap text-center">
                             Селекционно-семеноводческий центр
-{{--                            <br>--}}
-{{--                            «Высокотехнологичный селекционно-питомниководческий комплекс плодовых культур Новороссии»--}}
+                            {{--<br>--}}
+                            {{--«Высокотехнологичный селекционно-питомниководческий комплекс плодовых культур Новороссии»--}}
                         </span>
+                        <div class="ico-link bg-white rounded-full w-[40px] h-[40px] sm:w-[60px] sm:h-[60px] flex items-center justify-center">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3.66667 1H17M17 1V14.3333M17 1L1 17" stroke="#252525" stroke-width="2"/>
+                            </svg>
+                        </div>
                     </a>
                 </div>
             </div>
 
         </div>
-        <div class="slider-button prev" title="Previous">
+        <div class="slider-button prev hidden" title="Previous">
             <i class="bi bi-chevron-left"></i>
         </div>
-        <div class="slider-button next" title="Next">
+        <div class="slider-button next hidden" title="Next">
             <i class="bi bi-chevron-right"></i>
         </div>
     </div>
