467a091b94b5350930ce4a4802a6d2f266dca378
Максим Сын Отца Своего <59877317+Segizmund@users.noreply.github.com>
2025-07-11 08:02:27 +0300

    11.07

--- resources/views/components/menu/topmenu.blade.php
+++ resources/views/components/menu/topmenu.blade.php
@@ -1,6 +1,12 @@
 <div class="box-upper-nav z-82 bg-stone-50">
     <nav class="nav flex align-center">
-        <div class="bvi-shortcode"><a href="#" class="bvi-open"><svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="bvi-svg-eye"><path fill="currentColor" d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z" class="bvi-svg-eye"></path></svg> </a></div>
+        <div class="bvi-shortcode">
+            <a href="#" class="bvi-open group">
+                <svg aria-hidden="true" width="21" height="19" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="bvi-svg-eye">
+                    <path class="bvi-svg-eye fill-[#5e5058] transition duration-300 ease-linear group-hover:fill-[#C10F1A]" d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"></path>
+                </svg>
+            </a>
+        </div>
 
         <a class="nav-link flex items-center" href="#">
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
