ee25695e92032dd33891cd8a6b11cddff5fd3fa2
Максим Сын Отца Своего <59877317+Segizmund@users.noreply.github.com>
2025-07-16 16:23:43 +0300

    16.07

---
+++ resources/views/components/sections/scholarships.blade.php
@@ -0,0 +1,52 @@
+<section id="scholarships" class="container custom px-2.5 2xl:px-0">
+    <div class="flex justify-between items-center mb-8">
+        <h2 class="font-bold text-3xl">
+            Стипендии МелГУ
+        </h2>
+        <a href="#" class="flex gap-2 items-center">
+            Подробнее 
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.33333 1H15M15 1V12.6667M15 1L1 15" stroke="#252525" stroke-width="2"/>
+            </svg>
+        </a>
+    </div>
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-5">
+        <div>
+            <div class="bg-white bg-[radial-gradient(42.01%_51.38%_at_50%_50%,rgba(0,0,0,0.25)_0%,rgba(0,0,0,0)_100%)] max-h-[250px] sm:max-h-[512px] lg:max-h-[284px] overflow-hidden">
+                <img src="{{asset('img/scholarships-1.png')}}" alt="" class="h-full max-h-[250px] sm:max-h-[512px] lg:max-h-[284px] object-cover w-full lg:w-auto">
+            </div>
+            <div class="bg-[#252525] p-5 flex justify-between gap-3 text-white lg:min-h-[184px] 2xl:min-h-[160px]">
+                <div class="flex gap-3 justify-between">
+                    Студентам имеющим по итогам промежуточной аттестации оценки «отлично» и (или) «хорошо», с учетом среднего бала:
+                    <div class="flex flex-col justify-center w-fit">
+                    <h3 class="font-bold text-3xl text-nowrap">от 4</h3>
+                    <h3 class="font-bold text-3xl text-nowrap">до 5</h3>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="bg-white bg-[radial-gradient(42.01%_51.38%_at_50%_50%,rgba(130,0,0,0.25)_0%,rgba(130,0,0,0)_100%)] max-h-[250px] sm:max-h-[512px] lg:max-h-[284px overflow-hidden">
+                <img src="{{asset('img/scholarships-2.png')}}" alt="" class="h-full max-h-[250px] sm:max-h-[512px] lg:max-h-[284px] object-cover w-full lg:w-auto">
+            </div>
+            <div class="bg-[#252525] p-5 flex justify-between gap-3 text-white lg:min-h-[184px] 2xl:min-h-[160px]">
+                <div class="flex gap-3 justify-between">
+                    Студентам имеющим по итогам промежуточной аттестации оценки «отлично» и (или) «хорошо», с учетом среднего бала:
+                    <div class="flex flex-col justify-center w-fit">
+                        <h3 class="font-bold text-3xl text-nowrap">= 5</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="bg-white bg-[radial-gradient(42.01%_51.38%_at_50%_50%,rgba(250,254,60,0.25)_0%,rgba(250,254,60,0)_100%)] max-h-[250px] sm:max-h-[512px] lg:max-h-[284px overflow-hidden">
+                <img src="{{asset('img/scholarships-3.png')}}" alt="" class="h-full max-h-[250px] sm:max-h-[512px] lg:max-h-[284px] object-cover w-full lg:w-auto relative lg:top-[55px] lg:scale-[1.4]">
+            </div>
+            <div class="bg-[#252525] p-5 flex justify-between gap-3 text-white lg:min-h-[184px] 2xl:min-h-[160px]">
+                <div class="">
+                    Студентам, имеющим достижения в какой-либо одной или нескольких областях деятельности (учебной, научно-исследовательской, общественной, культурно- творческой и спортивной)
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
