26fdbf83c353ac04cbd31f70f1e26da9808f2595
languder2 <163087417+languder2@users.noreply.github.com>
2025-07-31 16:22:58 +0300

    w

--- resources/views/components/events/list-by-category.blade.php
+++ resources/views/components/events/list-by-category.blade.php
@@ -6,7 +6,7 @@
     @foreach($list as $item)
         <a
             href="{{ $item->link }}"
-            class="min-h-100 relative bg-cover block drop-shadow-md rounded-md cursor-pointer hover:-mt-2px hover:mb-2px duration-200"
+            class="h-100 relative bg-cover block drop-shadow-md rounded-md cursor-pointer hover:-mt-2px hover:mb-2px duration-200"
            style="background-image: url('{{ $item->preview->src }}')"
         >
             <div class="absolute inset-x-3 bottom-3 backdrop-blur-3xl bg-black/20 text-white px-4 py-3">
--- resources/views/components/template/left-side-offset.blade.php
+++ resources/views/components/template/left-side-offset.blade.php
@@ -1,13 +1,18 @@
 <div class="side_offset">
     <ul class="box-side-social-link">
         <li>
-            <a href="#" class="social-link">
+            <a href="https://vk.com/mlt_mgu" class="social-link" target="_blank">
                 <i class="fab fa-vk" aria-hidden="true"></i>
             </a>
         </li>
         <li>
-            <a href="#" class="social-link">
+            <a href="https://t.me/mgu_mlt" class="social-link" target="_blank">
                 <i class="bi bi-telegram"></i>
+            </a>
+        </li>
+        <li>
+            <a href="https://rutube.ru/channel/36418660/" class="social-link ps-3" target="_blank">
+                <img src="{{ asset('img/ico/Minilogo_RUTUBE_dark_color.png') }}" alt="rutube" class="h-3 inline-block -mt-1 mb-1" >
             </a>
         </li>
 
