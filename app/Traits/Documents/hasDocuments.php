874c78abebb74417e16bd2b94c65d4a3b66da78a
languder2 <163087417+languder2@users.noreply.github.com>
2025-12-17 16:39:05 +0300

    w

--- app/Traits/Documents/hasDocuments.php
+++ app/Traits/Documents/hasDocuments.php
@@ -7,17 +7,6 @@ trait HasDocuments
 
     protected static function bootHasDocuments():void
     {
-        static::retrieved(function ($model) {
-            $model->generate(
-                [
-                    'admin' => 'relation:documents:admin',
-                ],
-                [
-                    $model->getTable(),
-                    $model->id
-                ]
-            );
-        });
     }
 
 }
