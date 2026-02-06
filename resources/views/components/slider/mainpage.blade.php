4b9168333bff18994c33ddbfc67ad0dd103efef8
Максим Сын Отца Своего <59877317+Segizmund@users.noreply.github.com>
2026-02-06 13:55:31 +0300

    новый слайд

--- resources/views/components/slider/mainpage.blade.php
+++ resources/views/components/slider/mainpage.blade.php
@@ -17,6 +17,13 @@
                         </a>
                     </div>--}}
                     <div class="slide">
+                        <a href="https://melsu.ru/podgotovitelnye-kursy" class="slide-link cursor-pointer" target="_blank">
+                            <img src="{{asset('img/slider/Podgot-2k.png')}}" alt="" class="hidden object-cover min-[1921px]:block w-max">
+                            <img src="{{asset('img/slider/Podgot-1920.png')}}" alt="" class="hidden sm:block">
+                            <img src="{{asset('img/slider/Podgot-mob.png')}}" alt="" class="block sm:hidden">
+                        </a>
+                    </div>
+                    <div class="slide">
                         <a href="https://max.ru/id9001010985_biz" class="slide-link cursor-pointer" target="_blank">
                             <img src="{{asset('img/slider/MAX_2K.jpg')}}" alt="" class="hidden object-cover min-[1921px]:block w-max">
                             <img src="{{asset('img/slider/MAX_1920.jpg')}}" alt="" class="hidden sm:block">
